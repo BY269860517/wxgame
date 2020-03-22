d78beae03f057998e009ca9004e6711bce9a973a
BY269860517 <269860517@qq.com>
2020-03-22 22:05:53 +0800

    w

--- wxgame/config/config.js
+++ wxgame/config/config.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
    var contextValus={
 	 contextPath:"http://192.168.0.105:9097/game/"
  };
-=======
- var contextValus={
-	 contextPath:"http://192.168.0.106:9097/game/"
- }
->>>>>>> a24fe538d8f565935b0df6ed0566db50e0778c06
