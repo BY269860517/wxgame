80da24001a68240cf07c63c6057996e795e68451
AsianSleep <dummyfeel@163.com>
2020-03-23 00:49:07 +0800

    提交代碼

--- wxgame/common/includecommon.js
+++ wxgame/common/includecommon.js
@@ -1,17 +1,14 @@
-
 document.writeln('<link href="../css/css.css" rel="stylesheet" type="text/css"/>');
 document.writeln('<link href="../css/scroll.css" rel="stylesheet" type="text/css"/>');
 document.writeln('<link href="../css/jquery.webui-popover.min.css" rel="stylesheet" type="text/css"/>');
 document.writeln('<link href="../css/bootstrap-date.css" rel="stylesheet" type="text/css" />');
 document.writeln('<link href="../css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />');
-document.writeln('<link href="../css/vue-easytable.css" rel="stylesheet" type="text/css" />');
 
+document.writeln('<script src="../js/vue.js" type="text/javascript"></script>');
 document.writeln('<script src="../js/jquery.min.js" type="text/javascript"></script>');
+document.writeln('<script src="../js/vue-resource-1.3.4.js" type="text/javascript"></script>');
 document.writeln('<script src="../js/select.js" type="text/javascript"></script>');
 document.writeln('<script src="../js/jquery.webui-popover.min.js" type="text/javascript"></script>');
 document.writeln('<script src="../js/bootstrap-data/bootstrap-datetimepicker.min.js" type="text/javascript"></script>');
 document.writeln('<script src="../js/bootstrap-data/bootstrap-datetimepicker.zh-CN.js" type="text/javascript"></script>');
-document.writeln('<script src="../js/bootstrap-data/bootstrap-datetimepicker.zh-CN.js" type="text/javascript"></script>');
-document.writeln('<script src="../js/vue.js" type="text/javascript"></script>');
-document.writeln('<script src="../js/vue-easytable.js" type="text/javascript"></script>');
 document.writeln('<script src="../config/config.js" type="text/javascript"></script>');
